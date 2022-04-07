793edfd89211bfd5909bb33c0b72aa16cf696810
nutnapat <napat34402@gmail.com>
2022-04-07 12:21:03 +0700

    add swagger

--- server.js
+++ server.js
@@ -13,13 +13,36 @@ connectDB();
 const hospitals = require('./routes/hospitals');
 const appointments = require('./routes/appointments');
 const auth = require('./routes/auth');
-const app=express();
 const mongoSanitize=require('express-mongo-sanitize');
 const helmet=require('helmet');
 const xss=require('xss-clean');
 const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const cors=require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const app=express();
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers:[
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Body Parser
 app.use(express.json());
